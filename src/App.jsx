84bca03054eb85bcadbbf0976cd0b5be5d4f4d3f
SamTo83 <Sam2thanh@hotmail.co.uk>
2021-12-13 16:39:18 +0000

    Created 3 containers for each page, created routing in App.jsx for the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './containers/pages/Home/Home';
+import About from './containers/pages/About/About';
+import Schedule from './containers/pages/Schedule/Schedule';
 
-function App() {
-  return (
+const  App = () => {
+  return ( 
+    <Router>
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/our-story" element={<About/>} />
+        <Route path="/event-schedule" element={<Schedule />} />
+        <Route path="*" element={<Home/>} />
+      </Routes>
     </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/containers/pages/Schedule/Schedule.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Schedule = () => {
+  return (
+    <div>
+
+      <h1>Schedule page</h1>
+    </div>
+  )
+}
+
+export default Schedule
