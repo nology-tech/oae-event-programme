ee5f0fabfc73bc380c7404caa7367490daeacbe6
Tomasz <mandalorian99@gmail.com>
2021-12-14 17:31:36 +0000

    component, styled,  variables and typography

---
+++ src/components/ViewScheduleButton/ViewScheduleButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./ViewScheduleButton.scss";
+// import { Link } from "react-router-dom";
+const ViewScheduleButton = () => {
+  return <button className="schedule-button">View Schedule</button>;
+};
+export default ViewScheduleButton;
