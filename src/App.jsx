0d696acda7338805fd71d9012af70b5e5533f3d1
Jasen <60624556+jasenscode@users.noreply.github.com>
2021-12-14 16:08:01 +0000

    Removed component import from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">Header</header>
     </div>
   );
 }
