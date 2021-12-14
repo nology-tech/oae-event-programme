b27d182f9dcae08f3f1237d31fee8ac048098941
Tomasz <mandalorian99@gmail.com>
2021-12-14 17:42:25 +0000

    added link

--- src/components/ViewScheduleButton/ViewScheduleButton.jsx
+++ src/components/ViewScheduleButton/ViewScheduleButton.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./ViewScheduleButton.scss";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 const ViewScheduleButton = () => {
-  return <button className="schedule-button">View Schedule</button>;
+  return (
+    <Link to="">
+      <button className="schedule-button">View Schedule</button>
+    </Link>
+  );
 };
 export default ViewScheduleButton;
