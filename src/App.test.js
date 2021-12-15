3cb72d3b8ff8add61622af59e5158784a8dd9a6e
Louise Wong <lw17122@bristol.ac.uk>
2021-12-15 15:46:23 +0000

    completed testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,13 @@
 import { render } from "@testing-library/react";
-import App from "./App";
+//import App from "./App";
+import About from "./pages/About/About"
 
-test("Renders App component", () => {
-  const { container } = render(<App />);
+// test("Renders App component", () => {
+//   const { container } = render(<App />);
+//   expect(container).toMatchSnapshot();
+// });
+
+test("Renders About component, photo and description should appear", () => {
+  const { container } = render(<About />);
   expect(container).toMatchSnapshot();
 });
