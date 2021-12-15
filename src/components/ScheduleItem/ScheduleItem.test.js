42acf6a88b066c4dd7301b1e825e474799a8a27a
Jonathan Radford <jonathanradford@live.co.uk>
2021-12-15 15:32:37 +0000

    Tests redone

--- src/components/ScheduleItem/ScheduleItem.test.js
+++ src/components/ScheduleItem/ScheduleItem.test.js
@@ -1,25 +1,33 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
+import { customRender } from "../../utils/testUtils"
 import userEvent from "@testing-library/user-event";
 import ScheduleItem from "./ScheduleItem"
 
+test("Renders App component with default page", () => {
+  const { container } = customRender(<ScheduleItem />);
+  expect(container).toMatchSnapshot();
+});
+
 it("should render the button to the form", () => {
-    render(<ScheduleItem />);
+    customRender(<ScheduleItem />);
     const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
   });
 
 it("showText should not render to the form on loan", () => {
-  render(<ScheduleItem />);
+  customRender(<ScheduleItem />);
   const showtext = screen.queryByText("lorem ipsum whatever");
   expect(showtext).not.toBeInTheDocument();
 })
 
 it("should change the picture on click of the button", () => {
-  render(<ScheduleItem />); 
+  customRender(<ScheduleItem />); 
   const button = screen.getByRole("button");
   userEvent.click(button)
   expect(button).toHaveAttribute("src", "chevron-up.png");
 })
+
+
 
 
 
