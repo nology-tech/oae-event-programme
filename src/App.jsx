b9c9dc9562aa57285223cd3651fbba75cfbd9651
Jasen <60624556+jasenscode@users.noreply.github.com>
2021-12-15 14:45:26 +0000

    finished event header details component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import './App.scss';
+import "./App.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Schedule from './pages/Schedule/Schedule';
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Schedule from "./pages/Schedule/Schedule";
+import EventHeaderDetails from "./assets/components/EventHeaderDetails/EventHeaderDetails";
 
-const  App = () => {
+const App = () => {
   const baseURL = "/oae-event-programme";
-  return ( 
+  return (
     <Router>
-    <div className="App">
-      <Routes>
-        <Route path={baseURL} element={<Home />}/>
-        <Route path={baseURL +"/our-story"} element={<About/>} />
-        <Route path={baseURL + "/event-schedule"} element={<Schedule />} />
-        <Route path={baseURL +"*"} element={<Home/>} />
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path={baseURL} element={<Home />} />
+          <Route path={baseURL + "/our-story"} element={<About />} />
+          <Route path={baseURL + "/event-schedule"} element={<Schedule />} />
+          <Route path={baseURL + "*"} element={<Home />} />
+        </Routes>
+        <EventHeaderDetails time="11.30am" date="Sun 21 Nov 2021" venue="London's King Place" location="London" />
+      </div>
     </Router>
-    
   );
 };
 
