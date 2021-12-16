a18d2e202e14391e97b3d58012771a122f894803
Louise Wong <lw17122@bristol.ac.uk>
2021-12-16 15:43:44 +0000

    added rule into eslintrc.json

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.scss";
-import ViewScheduleButton from "../../components/ViewScheduleButton/ViewScheduleButton";
+import ViewScheduleButton from "../../Components/ViewScheduleButton/ViewScheduleButton";
 
 const Home = () => {
   return (
