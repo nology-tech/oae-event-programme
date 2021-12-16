7315425fdd43584f34e673eee1174b8884091f77
Ankita-Manmoandas <ankita.gandhim09@gmail.com>
2021-12-16 15:48:25 +0000

    added request made by dans review

--- src/components/TitleArticle/TitleArticle.test.js
+++ src/components/TitleArticle/TitleArticle.test.js
@@ -1,17 +1,17 @@
-import { render } from "@testing-library/react";
+import { customRender } from "../../utils/testUtils";
 import TitleArticle from "./TitleArticle";
 
 test("Renders TitleArticle component", () => {
-  const { container } = render(<TitleArticle />);
+  const { container } = customRender(<TitleArticle />);
   expect(container).toMatchSnapshot();
 });
 
 test("Header title is 'Our Instruments'", () => {
-  const { container } = render(<TitleArticle headerTitle="Our Instruments" />);
+  const { container } = customRender(<TitleArticle headerTitle="Our Instruments" />);
   expect(container).toMatchSnapshot();
 });
 
 test("Article text should say 'Article text'", () => {
-  const { container } = render(<TitleArticle articleText="Article text" />);
+  const { container } = customRender(<TitleArticle articleText="Article text" />);
   expect(container).toMatchSnapshot();
 });
