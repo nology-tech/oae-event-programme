8ba567f5bd45322ad38bda58c91ad2e41789e1f2
Tomasz <mandalorian99@gmail.com>
2021-12-16 15:33:20 +0000

    added paragraph container, styled

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Home.scss";
 import ViewScheduleButton from "../../components/ViewScheduleButton/ViewScheduleButton";
+import ParagraphComponent from "../../components/ParagraphComponent/ParagraphComponent";
 
 const Home = () => {
   return (
     <div className="home">
       <h1>Home page</h1>
+
+      <ParagraphComponent />
       <ViewScheduleButton />
     </div>
   );
