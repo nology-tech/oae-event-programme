7026a5d3868eb21735904d68652247cbe7c3acdc
Jasen <60624556+jasenscode@users.noreply.github.com>
2022-01-04 16:32:47 +0000

    removed screen import from testing file as unused

--- src/components/YoutubeEmbed/YoutubeEmbed.test.js
+++ src/components/YoutubeEmbed/YoutubeEmbed.test.js
@@ -1,6 +1,5 @@
 import YoutubeEmbed from "./YoutubeEmbed";
 import { customRender } from "../../utils/testUtils";
-import { screen } from "@testing-library/react";
 
 it("should render the YouTube embed component", () => {
   const { container } = customRender(<YoutubeEmbed embedId="q6EoRBvdVPQ" />);
