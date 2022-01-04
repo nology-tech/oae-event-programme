7b30532d3981b8e694d3b5149c2198cb8b3bceb1
SamTo83 <Sam2thanh@hotmail.co.uk>
2022-01-04 15:10:09 +0000

    resize the icons and re-aligned the title

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,47 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import HomeIcon from '../../assets/images/home.svg';
-import AboutIcon from '../../assets/images/info.svg';
-import ScheduleIcon from '../../assets/images/schedule.svg';
-import './Navbar.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import HomeIcon from "../../assets/images/home.svg";
+import AboutIcon from "../../assets/images/info.svg";
+import ScheduleIcon from "../../assets/images/schedule.svg";
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
     <div>
-      <nav> 
-        <div>
-        <Link 
-          to="/event-schedule"><img 
-          className="nav__icon" 
-          src={ScheduleIcon} 
-          alt="schedule icon" />
-        </Link>
-        <span>SCHEDULE</span>
+      <nav className="navbar">
+        <div className="navbar__content">
+          <Link to="/event-schedule">
+            <img
+              className="navbar__icon"
+              src={ScheduleIcon}
+              alt="schedule icon"
+            />
+          </Link>
+          <span className="navbar__title">SCHEDULE</span>
         </div>
-        <div>
-        <Link 
-          to="/"><img 
-          className="nav__icon" 
-          src={HomeIcon} 
-          alt="home icon" />
-        </Link>
-        <span>HOME</span>
-        </div>
-        
-
-        <div>
-        <Link 
-          to="/our-story"><img 
-          className="nav__icon" 
-          src={AboutIcon} 
-          alt="about icon" />
-        </Link>
-        <span>ABOUT</span>
+        <div className="navbar__content">
+          <Link to="/">
+            <img className="navbar__icon" src={HomeIcon} alt="home icon" />
+          </Link>
+          <span className="navbar__title">HOME</span>
         </div>
 
+        <div className="navbar__content">
+          <Link to="/our-story">
+            <img className="navbar__icon" src={AboutIcon} alt="about icon" />
+          </Link>
+          <span className="navbar__title">ABOUT</span>
+        </div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
