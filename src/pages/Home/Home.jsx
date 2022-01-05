bbe75bd2589f02a659c3d391d0818df004088e65
noah <noah.serafin04@gmail.com>
2022-01-05 15:20:24 +0000

    initialRequest

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,15 @@ import "./Home.scss";
 import ViewScheduleButton from "../../components/ViewScheduleButton/ViewScheduleButton";
 import Paragraph from "../../components/Paragraph/Paragraph";
 import EventHeaderDetails from "../../components/EventHeaderDetails/EventHeaderDetails";
-import YoutubeEmbed from "../../components/YoutubeEmbed/YoutubeEmbed";
+import HeaderImageVideoContainer from "../../components/HeaderImageVideoContainer/HeaderImageVideoContainer";
+import placeHolderImg from "../../assets/images/image33.png";
 
 const Home = () => {
   return (
     <div className="home">
       <h1 className="home__header">Home page</h1>
       <EventHeaderDetails time="11.30am" date="Sun 21 Nov 2021" venue="South Bank" location="London" />
-      <YoutubeEmbed embedId="q6EoRBvdVPQ" />
+      <HeaderImageVideoContainer imgSrc={placeHolderImg}/>
       <Paragraph text="this is some paragraph" />
       <ViewScheduleButton />
     </div>
