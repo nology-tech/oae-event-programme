5684077e224c5d9f931e2794bdd2392b19ea3c02
Ankita-Manmoandas <ankita.gandhim09@gmail.com>
2022-01-05 12:26:56 +0000

    edited jsx file (span->h1)

--- src/components/EventHeaderTitle/EventHeaderTitle.jsx
+++ src/components/EventHeaderTitle/EventHeaderTitle.jsx
@@ -4,7 +4,7 @@ const EventHeaderTitle = (props) => {
   const { title, subtitle } = props;
   return (
     <div className="header-home">
-      <span className="header-home__title">{title}</span>
+      <h1 className="header-home__title">{title}</h1>
       <span className="header-home__subtitle">{subtitle}</span>
     </div>
   );
