6da654cc57e81b3d9ce3b17eb0836a06ebc0d3e3
Howun Lam <howunlam96@gmail.com>
2022-01-05 16:42:32 +0000

    Added background colour. Tested href for links.

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,19 +1,24 @@
 
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import Navbar from "./Navbar";
-import { Link } from 'react-router';
+
 import { customRender } from "../../utils/testUtils";
+// import userEvent from "@testing-library/user-event";
+// import { container } from "react-router-dom";
 
 test("The links are contain in the page", () => {
   customRender(<Navbar/>);
-  const link = screen.getAllByRole('link');    
-  expect(link).not.toBeNull();
-})
+  // links is an array of three link components
+  const links = screen.getAllByRole('link');    
 
-test("Home Link goes to home page", () => {
-  const {container} = customRender(<Navbar/>);
-  const link = screen.getByRole('home-link');    
-  expect(container.innerHTML).toMatch('Home page');
-})
+  // check each of these links has the correct href value. e.g.'/','/event-schedule','/our-story'
 
+  // check RTL docs for how to get the href value of the link (which is an <a> tag)
+  expect(links[0]).toHaveAttribute("href", '/event-schedule');
+  expect(links[1]).toHaveAttribute("href", '/');
+  expect(links[2]).toHaveAttribute("href", '/our-story')
+  // toHaveAttribute
+
+  
+})
 
