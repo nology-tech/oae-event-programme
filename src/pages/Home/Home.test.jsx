3aed4a9454f511510242983e93170f251da3b31e
khalidhersi <cipherpolfoundation@gmail.com>
2022-01-05 15:39:13 +0000

    home test created

--- src/pages/Home/Home.test.jsx
+++ src/pages/Home/Home.test.jsx
@@ -3,8 +3,9 @@ import Home from "./Home";
 
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
-  useParams: jest.fn().mockReturnValue({ eventId: 1234 }),
-  useRouteMatch: () => ({ url: "/oae-event-programme/1234" }),
+  useParams: () => ({
+    eventId: "6bb8ffe8-6e5e-4abc-be7f-b05b749849cf",
+  }),
 }));
 
 test("Test", () => {
