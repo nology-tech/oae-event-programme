37c9341dc87c6c297314b0fd9c6fbe4fa4f9e8f8
Josie Newman <82321832+JosieAilsa@users.noreply.github.com>
2022-01-05 17:07:21 +0000

    layout component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,17 +4,21 @@ import ViewScheduleButton from "../../components/ViewScheduleButton/ViewSchedule
 import Paragraph from "../../components/Paragraph/Paragraph";
 import EventHeaderDetails from "../../components/EventHeaderDetails/EventHeaderDetails";
 import YoutubeEmbed from "../../components/YoutubeEmbed/YoutubeEmbed";
+import Layout from "../../components/Layout/Layout";
 
 const Home = () => {
+
   return (
-    <div className="home">
+    <Layout>
+      <div className = "home">
       <h1 className="home__header">Home page</h1>
       <EventHeaderDetails time="11.30am" date="Sun 21 Nov 2021" venue="South Bank" location="London" />
       <YoutubeEmbed embedId="q6EoRBvdVPQ" />
       <Paragraph text="this is some paragraph" />
       <ViewScheduleButton />
-      <Navbar/>
-    </div>
+      {/* <Navbar/> */}
+      </div>
+    </Layout>
   );
 };
 
