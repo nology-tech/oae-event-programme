492009883c4cae49fa06d70416e16d58c8333cf9
Howun Lam <howunlam96@gmail.com>
2022-01-06 15:15:00 +0000

    Layout test added

--- src/components/Layout/Layout.test.jsx
+++ src/components/Layout/Layout.test.jsx
@@ -1,5 +1,11 @@
-{
-  /* <Layout>
-  <p>sample children</p>
-</Layout> */
-}
+import Layout from "./Layout";
+import { customRender } from "../../utils/testUtils";
+
+test("Renders Layout component with default page", () => {
+  const { container } = customRender(
+    <Layout>
+      <p>sample children</p>
+    </Layout>
+  );
+  expect(container).toMatchSnapshot();
+});
