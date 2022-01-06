7ba81862ba2074b565b977bb6c353ad81820f5be
Tomasz <mandalorian99@gmail.com>
2022-01-06 16:16:56 +0000

    updated about test

--- src/pages/About/About.test.js
+++ src/pages/About/About.test.js
@@ -1,6 +1,13 @@
 import { customRender } from "../../utils/testUtils";
 import About from "./About";
 
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: () => ({
+    eventId: "6bb8ffe8-6e5e-4abc-be7f-b05b749849cf",
+  }),
+}));
+
 test("Renders photo component in the About page", () => {
   const { container } = customRender(<About />);
   expect(container).toMatchSnapshot();
