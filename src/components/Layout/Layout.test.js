9c3fc615fcae989b39c37a91cdc00590580a8459
Louise Wong <lw17122@bristol.ac.uk>
2022-01-06 17:00:35 +0000

    fixed testing for font type & formated

--- src/components/Layout/Layout.test.js
+++ src/components/Layout/Layout.test.js
@@ -1,22 +1,21 @@
 import Layout from "./Layout";
 import { customRender } from "../../utils/testUtils";
 
-test("Renders the Layout component with default page", () => {
+test("Renders Layout component with default page", () => {
   const { container } = customRender(
-    <Layout>
-      <p>Sample children</p>
+    <Layout eventId="123">
+      <p>sample children</p>
     </Layout>
   );
-
   expect(container).toMatchSnapshot();
 });
 
-test("Ensure the Layout div has a className", () => {
-  const { container } = customRender(
-    <Layout>
-      {" "}
-      <p>Sample children</p>{" "}
-    </Layout>
-  );
-  expect(container.firstChild).toHaveClass("modern");
+test("default font type for Layout is modern", () => {
+  const { container } = customRender(<Layout />);
+  expect(container.firstChild.classList.contains("modern")).toBe(true);
+});
+
+test("font type will change to classic", () => {
+  const { container } = customRender(<Layout fontType={2} />);
+  expect(container.firstChild.classList.contains("classic")).toBe(true);
 });
