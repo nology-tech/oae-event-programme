d62c8349ca90aded7fa6952a746d2dc8b243f09c
Jonathan Radford <jonathanradford@live.co.uk>
2022-01-10 17:19:13 +0000

    Navbar First Edit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,58 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import HomeIcon from "../../assets/images/home.svg";
-import AboutIcon from "../../assets/images/info.svg";
-import ScheduleIcon from "../../assets/images/schedule.svg";
+import { NavLink } from "react-router-dom";
+import AboutIcon from "../NavbarIcon/AboutIcon";
+import ScheduleIcon from "../NavbarIcon/ScheduleIcon";
+import HomeIcon from "../NavbarIcon/Home";
 import "./Navbar.scss";
 
 const Navbar = (props) => {
   const { eventId } = props;
-  const [bg, changeBGColor] = React.useState(1);
 
   return (
     <nav className="navbar">
       <div className="navbar__content">
-        <Link
-          className="navbar__schedule-link"
-          onClick={() => changeBGColor(1)}
-          style={{
-            backgroundColor: bg === 1 ? "black" : "red",
-          }}
+        <NavLink
+          style={({ isActive }) => ({ fill: isActive ? "black" : "#CACBCD" })}
+          //className="navbar__schedule-link"
           to={`/${eventId}/event-schedule`}
         >
-          <img
-            className="navbar__icon"
-            src={ScheduleIcon}
-            alt="schedule icon"
-          />
-        </Link>
+          <ScheduleIcon />
+        </NavLink>
         <span className="navbar__title">SCHEDULE</span>
       </div>
       <div className="navbar__content">
-        <Link
-          className="navbar__home-link"
-          onClick={() => changeBGColor(2)}
-          style={{
-            backgroundColor: bg === 2 ? "black" : "yellow",
-          }}
-          to={`/${eventId}`}
+        <NavLink
+          style={({ isActive }) => ({ fill: isActive ? "black" : "#CACBCD" })}
+          //className="navbar__home-link"
+          to={`/${eventId}/home`}
         >
-          <img className="navbar__icon" src={HomeIcon} alt="home icon" />
-        </Link>
+          <HomeIcon />
+        </NavLink>
         <span className="navbar__title">HOME</span>
       </div>
 
       <div className="navbar__content">
-        <Link
+        <NavLink
           className="navbar__about-link"
-          onClick={() => changeBGColor(3)}
-          style={{
-            backgroundColor: bg === 3 ? "black" : "green",
-          }}
           to={`/${eventId}/our-story`}
+          style={({ isActive }) => ({ fill: isActive ? "black" : "#CACBCD" })}
         >
-          <img className="navbar__icon" src={AboutIcon} alt="about icon" />
-        </Link>
+          {/* <img className="navbar__icon" src={AboutIcon} alt="about icon" /> */}
+          <AboutIcon />
+        </NavLink>
         <span className="navbar__title">ABOUT</span>
       </div>
     </nav>
