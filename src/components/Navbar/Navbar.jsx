cc9fe17dc6a2e69ff113c628cc58142c98d3efd4
Jonathan Radford <jonathanradford@live.co.uk>
2022-01-10 15:19:28 +0000

    First Commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,11 +7,17 @@ import "./Navbar.scss";
 
 const Navbar = (props) => {
   const { eventId } = props;
+  const [bg, changeBGColor] = React.useState(1);
+
   return (
     <nav className="navbar">
       <div className="navbar__content">
         <Link
           className="navbar__schedule-link"
+          onClick={() => changeBGColor(1)}
+          style={{
+            backgroundColor: bg === 1 ? "black" : "red",
+          }}
           to={`/${eventId}/event-schedule`}
         >
           <img
@@ -23,14 +29,28 @@ const Navbar = (props) => {
         <span className="navbar__title">SCHEDULE</span>
       </div>
       <div className="navbar__content">
-        <Link className="navbar__home-link" to={`/${eventId}`}>
+        <Link
+          className="navbar__home-link"
+          onClick={() => changeBGColor(2)}
+          style={{
+            backgroundColor: bg === 2 ? "black" : "yellow",
+          }}
+          to={`/${eventId}`}
+        >
           <img className="navbar__icon" src={HomeIcon} alt="home icon" />
         </Link>
         <span className="navbar__title">HOME</span>
       </div>
 
       <div className="navbar__content">
-        <Link className="navbar__about-link" to={`/${eventId}/our-story`}>
+        <Link
+          className="navbar__about-link"
+          onClick={() => changeBGColor(3)}
+          style={{
+            backgroundColor: bg === 3 ? "black" : "green",
+          }}
+          to={`/${eventId}/our-story`}
+        >
           <img className="navbar__icon" src={AboutIcon} alt="about icon" />
         </Link>
         <span className="navbar__title">ABOUT</span>
