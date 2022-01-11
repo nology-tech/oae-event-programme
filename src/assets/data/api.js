d6591b01626bcf8013a8de64880ee7a559fd7d78
Tomasz <mandalorian99@gmail.com>
2022-01-11 08:56:02 +0000

    last commit

--- src/assets/data/api.js
+++ src/assets/data/api.js
@@ -1,15 +1,9 @@
 const url = `https://oae-mock-api.herokuapp.com/events/${id}`;
 
-export const getEvent = async (id) => {
+export const getEvent = async () => {
   fetch(url)
     .then((res) => res.json())
     .then((event) => {
       console.log(event);
     });
 };
-
-async function getUserAsync() {
-  let response = await fetch(url);
-  let data = await response.json();
-  return data;
-}
