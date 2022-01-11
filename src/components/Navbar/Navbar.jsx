5d6d5351b626db9a2ee086fa9467af13954d7277
Jonathan Radford <jonathanradford@live.co.uk>
2022-01-11 08:20:26 +0000

    Navbar links changing colour

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,35 +12,35 @@ const Navbar = (props) => {
     <nav className="navbar">
       <div className="navbar__content">
         <NavLink
-          style={({ isActive }) => ({ fill: isActive ? "black" : "#CACBCD" })}
-          //className="navbar__schedule-link"
+          // style={({ isActive }) => ({ fill: isActive ? "black" : "#CACBCD" })}
+          className={({ isActive }) => (isActive ? "active" : "inactive")}
           to={`/${eventId}/event-schedule`}
         >
           <ScheduleIcon />
+          <span className="navbar__title">SCHEDULE</span>
         </NavLink>
-        <span className="navbar__title">SCHEDULE</span>
       </div>
       <div className="navbar__content">
         <NavLink
-          style={({ isActive }) => ({ fill: isActive ? "black" : "#CACBCD" })}
-          //className="navbar__home-link"
+          //     style={({ isActive }) => ({ fill: isActive ? "black" : "#CACBCD" })}
+          className={({ isActive }) => (isActive ? "active" : "inactive")}
           to={`/${eventId}/home`}
         >
           <HomeIcon />
+          <span className="navbar__title">HOME</span>
         </NavLink>
-        <span className="navbar__title">HOME</span>
       </div>
 
       <div className="navbar__content">
         <NavLink
-          className="navbar__about-link"
+          className={({ isActive }) => (isActive ? "active" : "inactive")}
           to={`/${eventId}/our-story`}
-          style={({ isActive }) => ({ fill: isActive ? "black" : "#CACBCD" })}
+          //   style={({ isActive }) => ({ fill: isActive ? "black" : "#CACBCD" })}
         >
           {/* <img className="navbar__icon" src={AboutIcon} alt="about icon" /> */}
           <AboutIcon />
+          <span className="navbar__title">ABOUT</span>
         </NavLink>
-        <span className="navbar__title">ABOUT</span>
       </div>
     </nav>
   );
