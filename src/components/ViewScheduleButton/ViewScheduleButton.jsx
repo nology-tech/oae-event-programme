0806de824cd31ad3329af4766a50b781b0804e1b
noah <noah.serafin04@gmail.com>
2022-01-11 15:28:11 +0000

    fixed link but may need a prop in future

--- src/components/ViewScheduleButton/ViewScheduleButton.jsx
+++ src/components/ViewScheduleButton/ViewScheduleButton.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const ViewScheduleButton = () => {
   return (
-    <Link to={"/event-schedule"} className="view-schedule-button">
+    <Link to={"/6bb8ffe8-6e5e-4abc-be7f-b05b749849cf/event-schedule"} className="view-schedule-button">
       View Schedule
     </Link>
   );
