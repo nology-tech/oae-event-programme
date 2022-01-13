6eed49c454eeb49e16b9206cc9c47c0e44d1d4eb
noah <noah.serafin04@gmail.com>
2022-01-13 17:23:53 +0000

    refactored paragraph ternary

--- src/components/Paragraph/Paragraph.jsx
+++ src/components/Paragraph/Paragraph.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import "./Paragraph.scss";
-import { Fragment } from "react/cjs/react.development";
 
 const Paragraph = (props) => {
   const { text, isCallOut } = props;
 
-  return (
-    <Fragment>
-      {isCallOut ? (
-        <p data-testid="paragraph" className="paragraph paragraph__callout">
-          {text}
-        </p>
-      ) : (
-        <p data-testid="paragraph" className="paragraph">
-          {text}
-        </p>
-      )}
-    </Fragment>
+  return (      
+        <p data-testid="paragraph" 
+        className={`paragraph ${
+          isCallOut ? " paragraph__callout" : ""
+        }`}> 
+        {text}
+        </p>  
   );
 };
 export default Paragraph;
