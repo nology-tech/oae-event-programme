26a0919175680d72e4594448a400e45cad155a70
Tomasz <mandalorian99@gmail.com>
2022-01-14 14:30:44 +0000

    fixed units, naming and test

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -3,11 +3,11 @@ import "./LoadingSpinner.scss";
 
 const LoadingSpinner = () => {
   return (
-    <div className="loading" data-testid="loading">
-      <div className="spinner">
-        <div className="bounce1"></div>
-        <div className="bounce2"></div>
-        <div className="bounce3"></div>
+    <div className="loading">
+      <div className="loading__spinner">
+        <div className="loading__bounce1"></div>
+        <div className="Loading__loading__bounce2"></div>
+        <div className="loading__bounce3"></div>
       </div>
       <h1 className="loading__heading">Loading</h1>
     </div>
--- src/components/LoadingSpinner/LoadingSpinner.test.js
+++ src/components/LoadingSpinner/LoadingSpinner.test.js
@@ -1,9 +1,8 @@
 import { customRender } from "../../utils/testUtils";
-import { screen } from "@testing-library/react";
 import LoadingSpinner from "./LoadingSpinner";
 
 it("should render the loading spinner", () => {
-  customRender(<LoadingSpinner />);
-  const loading = screen.getByTestId("loading");
-  expect(loading).toMatchSnapshot();
+  const { container } = customRender(<LoadingSpinner />);
+
+  expect(container).toMatchSnapshot();
 });
