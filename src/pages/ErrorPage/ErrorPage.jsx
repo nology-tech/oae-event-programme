6677bc4e938956c40333b1f7198c12f4209a7678
Tomasz <mandalorian99@gmail.com>
2022-01-17 17:11:55 +0000

    added error page styling

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import "./ErrorPage.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./ErrorPage.scss";
 
-const ErrorPage = (props) => {
-    const {eventId} = props;
-    return (
-        <div>
-            <h1>Oops. Looks like this page doesnt exist.</h1>
-            <Link to={`/${eventId}/home`}>Back to Website</Link>
-        </div>
-    )
-}
+const ErrorPage = () => {
+  return (
+    <div className="error-page">
+      <h1 className="error-page__heading">
+        Oops. Looks like this page doesnt exist.
+      </h1>
+      <h2 className="error-page__subheading">Error 404</h2>
+      <Link className="error-page__button" to={"//oae.co.uk/"}>
+        Back to Website
+      </Link>
+    </div>
+  );
+};
 
 export default ErrorPage;
