9b56068e0fa5376a86617f470b27406537ad9af4
Jasen <60624556+jasenscode@users.noreply.github.com>
2022-01-20 17:06:35 +0000

    removed screen import from test file

--- src/components/SettingsItemToggle/SettingsItemToggle.test.js
+++ src/components/SettingsItemToggle/SettingsItemToggle.test.js
@@ -1,6 +1,5 @@
 import { customRender } from "../../utils/testUtils";
 import SettingsItemToggle from "./SettingsItemToggle";
-import { screen } from "@testing-library/react";
 
 test("renders settingsItemToggle component", () => {
   const { container } = customRender(<SettingsItemToggle />);
